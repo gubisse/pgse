c8b366af7f1256342ba053846670863a3d648cde
Gonsalves Ubisse <goncalvesalbertoubisse@gmail.com>
2025-02-17 19:45:59 +0200

    Configurar 008

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,13 @@
-import { component$, useSignal, $ } from "@builder.io/qwik";
-
-import { PainelPrincipal } from "../components/painel-principal";
+import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  const componenteAtual = useSignal("tecnico"); // Controla o componente ativo
-  const menuAberto = useSignal(false); // Controla o estado do menu (aberto ou fechado)
-
-  // Função para alternar o estado do menu
-  const alternarMenu = $(() => {
-    menuAberto.value = !menuAberto.value;
-  });
-
-  const userName = "João Silva"; // Aqui você pode dinamicamente pegar o nome do usuário
-
 
   return (
     <div class="d-flex flex-column flex-md-row ">
       <div class="">
         Oi Ubisie como esta tsx
       </div>
-      
+
     </div>
   );
 });
